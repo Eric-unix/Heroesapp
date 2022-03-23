14506ae9aa455d12d584d352ccbb1217365f8fdf
EricUnix <you@example.com>
2022-03-22 19:04:45 -0600

    Lista de Heroes

--- src/routers/DashboardRutes.js
+++ src/routers/DashboardRutes.js
@@ -10,16 +10,17 @@ export const DashboardRutes = () => {
   return (
     <>
         <Navbar/>
-
-        <Routes>
-            <Route path="marvel" element={<MarvelScreen />} />
-            <Route path="dc" element={<DcScreen />} />
-
-            <Route path="search" element={<SearchScreen/>} />
-            <Route path="hero" element={<HeroScreen/>} />
-
-            <Route path="/" element={<MarvelScreen/>} />
-        </Routes>   
+        <div className='container'>
+             <Routes>
+                 <Route path="marvel" element={<MarvelScreen />} />
+                 <Route path="dc" element={<DcScreen />} />
+                 <Route path="search" element={<SearchScreen/>} />
+                 <Route path="hero" element={<HeroScreen/>} />
+                 <Route path="/" element={<MarvelScreen/>} />
+             </Routes>   
+          
+        </div>
+ 
     </>
   )
 }
