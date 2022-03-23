f227922b927d9debf8dc48fabf699ba3e29b8d6f
EricUnix <you@example.com>
2022-03-22 20:27:37 -0600

    mostrar listado de heroes

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,5 +1,6 @@
-import { useForm } from "../../hooks/useForm"
-
+import { useForm } from '../../hooks/useForm'
+import { getHeroesByName } from '../../selectors/getHeroesByName';
+import { HeroCard } from '../hero/HeroCard';
 
 export const SearchScreen = () => {
   const [ formValues, handleInputChange] = useForm({
@@ -7,6 +8,8 @@ export const SearchScreen = () => {
   })
 
   const { searchText }  = formValues;
+  const heroesFileted = getHeroesByName('ALGO POR AQUI')
+
 
   const handleSearch = (e) => {
     e.preventDefautl();
@@ -45,8 +48,20 @@ export const SearchScreen = () => {
               </form>
 
             </div>
+            <div className='col-7'>
+              <h4>Resultados</h4>
+              <hr />
 
+              {
+                heroesFileted.map(hero =>(
+                  <HeroCard 
+                     key={ hero.id }
+                     {...hero }
+                  />
+                ))
+              }
 
+            </div>
           </div>
       </>
     )
