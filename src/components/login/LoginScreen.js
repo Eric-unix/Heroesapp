739841f28f9e85ab951656ba0e272f1652531572
EricUnix <you@example.com>
2022-03-22 18:47:37 -0600

    navigate push/ replace - useNavigate

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,27 @@
-
+import { useNavigate } from "react-router-dom";
 
 export const LoginScreen = () => {
-    return (
-      <div>
-          <h1>LoginScreen</h1>
-      </div>
-    )
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/marvel', {
+      replace: true
+    })
   }
+
+  return (
+    <div className="container mt-5">
+    <h1>LoginScreen</h1>
+    <hr />
+
+    <button 
+    className="btn btn-primary"
+    onClick={ handleLogin }
+    >
+    Login
+
+      </button>
+   </div>
+  )
+}
